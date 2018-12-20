29353e0d304a39ad026ad2e0167b233738fb906c
Shashi Deshetti <deshetti@gmail.com>
2018-12-20 13:36:21 -0600

    Added the port to docker to be exposed on AWS

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,5 @@ RUN npm run build
 # The files are copied to /app/build from the previous steps
 # This is to run nginx
 FROM nginx
+EXPOSE 80
 COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Shashi Deshetti & Punam Malani
+            Shashi Deshetti, Punam Malani & More!
           </p>
           <a
             className="App-link"
